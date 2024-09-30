50b7f16709c09d3e45019fe0c1cfb9fd9597ea04
Tuca <guga_camil@hotmail.com>
2024-09-30 15:48:46 -0300

    Tratamento de exceção na inserção de usuários

--- src/main/java/com/tucadev/course/services/UserService.java
+++ src/main/java/com/tucadev/course/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.tucadev.course.entities.User;
 import com.tucadev.course.repositories.UserRepository;
+import com.tucadev.course.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -20,7 +21,7 @@ public class UserService {
 	
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 	
 	public User insert(User obj) {
